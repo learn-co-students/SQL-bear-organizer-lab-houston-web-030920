80baec65011a3ec584fbc869a145eb38203f3fc8
AidanMcB <aidankmcbride@gmail.com>
2020-03-19 17:49:33 -0500

    Done.

--- lib/seed.sql
+++ lib/seed.sql
@@ -7,3 +7,11 @@ INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
 (6,"Grinch", 2, "M", "Black", "Grinchy", 1),
 (7,"Wendy", 6, "F", "Blue", "naive", 1),
 (8,null, 20, "M", "black", "aggressive", 0);
+
+
+-- Get the tests in `spec/select_spec.rb` to pass. Note that for this section, the database will be seeded 
+-- with external data from the `lib/seed.sql` file so don't expect it to reflect the data you added above. 
+-- Note: Since it's a Ruby file, write your queries as strings in the `lib/sql_queries.rb`.
+
+-- You may be expected to use SQL statements that 
+-- you're not particularly familiar with. Make sure you use the resources and Google to find the right statements.
