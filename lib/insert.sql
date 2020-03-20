30712d3a27ad2d93e140103f0c7f2d320f90103d
angeloxenakis <angeloxenakis@gmail.com>
2020-03-19 21:47:24 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 5, "male", "brown", "calm", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 7, "male", "brown", "agressive", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 15, "female", "black", "calm", false);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 12, "male", "white", "aggressive", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 20, "female", "white", "calm", false);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 16, "male", "green", "angry", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 4, "female", "black", "calm", true);
+INSERT INTO bears (age, gender, color, temperament, alive) VALUES (11, "male", "white", "angry", true);
